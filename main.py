2db4d09c466590ec66571d166a16f263c60d43e9
Jeff Sheary <jeffsheary@gmail.com>
2018-10-18 16:21:31 -0500

    done

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABSE_URI'] = 'mysql+pymysql://buld-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
-    id = db.Column(db.Integer.primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
 
